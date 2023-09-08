617ec331c801784d185d494cabd856a475f127b8
George Gorospe <george.e.gorospe@gmail.com>
2023-09-07 22:04:50 -0700

    continued progress on training programs

--- Training/Dataloader_intro.py
+++ Training/Dataloader_intro.py
@@ -11,7 +11,7 @@ from torch.utils.data import DataLoader
 # The training process starts with selecting a folder containing the data for training.
 
 # Get the current working directory (cwd)
-datasets_filepath = "/Users/ggoro/Documents/Robotics/Datasets/"
+datasets_filepath = "/home/pathfinder/Datasets/"
 
 
 dataset_list = [] # List of our datasets, empty for now
@@ -21,27 +21,34 @@ for filename in os.listdir(datasets_filepath):
 print("Available Datasets: ") 
 print(dataset_list)
 
-selected_data = dataset_list[0] # Default is the first dataset
+selected_data = dataset_list[2] # Default is the first dataset
 print("Selected data for training: " + selected_data )
 selected_dataset_filepath = os.path.join(datasets_filepath, selected_data)
 
 # Loading the selected folder of datapoints into a custom dataset, Regression_dataset, created for image regression
-dataset = Regression_dataset(selected_dataset_filepath,transform=None, random_hflip=False)
-print("Data points in the dataset: " + str(dataset.__len__())) # Size of the dataset
+train_dataset = Regression_dataset(selected_dataset_filepath,transform=None, random_hflip=False)
+print("Data points in the training dataset: " + str(train_dataset.__len__())) # Size of the dataset
 
 # Consider using a regular pytorch dataloader here, (take advantage of the index and the batching)
+# Consider creating a verfied dataset to act as the test data.
 
 # Prototype:
-train_dataloader = DataLoader(dataset, batch_size=100)
+#train_dataloader = DataLoader(train_dataset, batch_size=100)
 
+# Mock training process, used to demonstrate the iteration of the dataset
 def simple_train(dataset):
-    i = 0
+    i = 0 # an index for the file number
+
+    # For loop iterating across the given datset, producing the image and the label
     for images, xy in enumerate(dataset):
 
         if i % 1000 == 999:
             print("i: " + str(i))
+            print("File name: " + str(images))
 
         i = i+1
     print(i)
 
-simple_train(dataset)
+simple_train(train_dataset)
+
+print(train_dataset[0])
--- Training/pytorch_intro.py
+++ Training/pytorch_intro.py
@@ -125,6 +125,7 @@ def test(dataloader, model, loss_fn):
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n")
 
 
+# start the training
 
 epochs = 5
 for t in range(epochs):
